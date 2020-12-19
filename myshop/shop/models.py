8367a622c1c957730314b3b6a50983823ae4fc92
wiky-avis <peta-holding@yandex.ru>
2020-12-19 20:56:13 +0300

    карточка товара

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    descriptions = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
