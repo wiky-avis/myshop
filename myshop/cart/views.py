371553a8784a1c16866cf1305b6d70825ee510ed
wiky-avis <peta-holding@yandex.ru>
2020-12-20 10:23:05 +0300

    обработчики для добавления, обновления и удаления товаров из корзины.

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
 
-# Create your views here.
+
+# обработчик для добавления товаров в корзину
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(
+            product=product, 
+            quantity=cd['quantity'], 
+            update_quantity=cd['update']
+        )
+    return redirect('cart:cart_detail')
+
+# Для удаления товара из корзины
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+# для страницы списка товаров добавленных в корзину
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html',{'cart': cart})
